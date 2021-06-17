cdcc372c6e7012e1384e91ff3683afa65b95641c
Rupesh Prajapati <39094963+Rupeshpra22@users.noreply.github.com>
2021-06-18 01:42:15 +0530

    setup production code

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const developer = require('./developer');
 
 app.use(express.json());
 
-let allowAccessOrigin = (req,res,next)=>{
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('build'));
+    app.get('*', (req, res) => {
+        req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    })
+}
+let allowAccessOrigin = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
-    res.header('Acces-Contorl-Allow-Methods','Content-Type','Authorization')
+    res.header('Acces-Contorl-Allow-Methods', 'Content-Type', 'Authorization')
     next();
 }
 app.use(allowAccessOrigin)
