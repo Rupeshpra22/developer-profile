af2fef9d8b176249935111f96e2ced95d4619bd9
Rupesh Prajapati <39094963+Rupeshpra22@users.noreply.github.com>
2021-06-18 02:21:54 +0530

    app.js changes

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const developer = require('./developer');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/*', (req, res) => {
-    console.log(path.join(__dirname+'/client/build/index.html'))
-    res.sendFile(path.join(__dirname+'/client/build/index.html'))
-})
+if (process.env.NODE_ENV === "production") {
+    app.get('/*', (req, res, next) => {
+        res.sendFile(path.join(__dirname + '/client/build/index.html'))
+        next()
+    })
+}
 
 let allowAccessOrigin = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
